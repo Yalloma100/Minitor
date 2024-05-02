a4d6969663e1e0f0be5cbabdfd4a9be1ccf2c18f
Yalloma100 <164032163+Yalloma100@users.noreply.github.com>
2024-05-02 17:33:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,18 +12,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 @bot.message_handler(commands=['start'])
 def start_message(message):
   bot.send_message(message.chat.id, 'Привіт!')
-
-# Запуск бота
-
-URL = 'https://nh-c1fe.onrender.com'  # Замените URL на адрес нужной веб-страницы
-current_hash = None
-
-def fetch_content(url):
-       response = requests.get(url)
-       soup = BeautifulSoup(response.content, 'html.parser')
-       return soup.get_text()  # Получаем текст страницы для сравнения
-
-while True:
+  while True:
        print("Проверка на изменения...")
        content = fetch_content(URL)
        new_hash = hashlib.sha256(content.encode('utf-8')).hexdigest()
@@ -35,5 +24,15 @@ while True:
            print("Обнаружены изменения!")
            current_hash = new_hash
            # Здесь можно добавить отправку уведомлений или другие действия
+
+# Запуск бота
+
+URL = 'https://nh-c1fe.onrender.com'  # Замените URL на адрес нужной веб-страницы
+current_hash = None
+
+def fetch_content(url):
+       response = requests.get(url)
+       soup = BeautifulSoup(response.content, 'html.parser')
+       return soup.get_text()  # Получаем текст страницы для сравнения
 
 bot.polling()
