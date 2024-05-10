1412a0c093cd5c094dd3825019f3cdea28ffc51e
Yalloma100 <164032163+Yalloma100@users.noreply.github.com>
2024-05-10 21:33:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-import requests
-from bs4 import BeautifulSoup
-import hashlib
-import time
+ import requests
+   from bs4 import BeautifulSoup
+   import hashlib
+   import time
 
-while True:
+   URL = 'http://example.com'  # Замените URL на адрес нужной веб-страницы
+   current_hash = None
+
+   def fetch_content(url):
+       response = requests.get(url)
+       soup = BeautifulSoup(response.content, 'html.parser')
+       return soup.get_text()  # Получаем текст страницы для сравнения
+
+   while True:
        print("Проверка на изменения...")
        content = fetch_content(URL)
        new_hash = hashlib.sha256(content.encode('utf-8')).hexdigest()
@@ -16,12 +24,4 @@ while True:
            current_hash = new_hash
            # Здесь можно добавить отправку уведомлений или другие действия
 
-# Запуск бота
-
-URL = 'https://pnv8vn88wsnaaxr6q8rkjn.streamlit.app/'  # Замените URL на адрес нужной веб-страницы
-current_hash = None
-
-def fetch_content(url):
-       response = requests.get(url)
-       soup = BeautifulSoup(response.content, 'html.parser')
-       return soup.get_text()  # Получаем текст страницы для сравнения
+       time.sleep(10)  # Проверка каждые 10 секунд
